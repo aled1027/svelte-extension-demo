075b689156c25b946452090df1a878e67fd6472b
Alex Ledger <alex@playgroundlabs.io>
2024-06-17 13:35:25 +0200

    Add code

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let searchInput: HTMLInputElement;
+  let searchInputValue = "";
+
+  onMount(async () => {
+    searchInput.focus();
+  });
+</script>
+
+<div class="popup">
+  <h1>Svelte Extension Demo</h1>
+  <input
+    bind:this={searchInput}
+    bind:value={searchInputValue}
+    type="text"
+    class="search-input"
+  />
+</div>
+
+<style>
+  .popup {
+    width: 640px;
+    height: 480px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
